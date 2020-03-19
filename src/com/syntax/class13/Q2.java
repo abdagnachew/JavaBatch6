51946071276c2d8719f431f035cf7e9c8c653c0b
abdagnachew <abdagnachew@gmail.com>
2020-03-18 21:08:54 -0400

    More Code for Repository

---
+++ src/com/syntax/class13/Q2.java
@@ -0,0 +1,26 @@
+package com.syntax.class13;
+
+import java.util.Scanner;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int number = input.nextInt();
+
+		for (int i = 0; i < number; i++) {
+			if (number == 0) {
+				System.out.println("Not a prime" );
+			} else {
+				System.out.println("prime" );
+			}
+			
+
+//		int given=7;
+//		boolean flag=true;
+
+		}
+	}
+}
